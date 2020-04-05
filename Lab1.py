6355e03b9b80a6dd6e5c84a3988351e06630c72e
Rakan-alsubhi <63181849+Rakan-alsubhi@users.noreply.github.com>
2020-04-05 10:28:46 +0300

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# > # Home Work1
+# >
+# > ### \begin{equation} f(x) = a_0 + \sum_{n=1}^{\infty} \bigl(a_n \cos\frac{n \pi x}{L} + b_n\sin\frac{n \pi x}{L}\bigr)  \end{equation}
+# >
+# > ------------------------------
+# >
+# > ### \begin{equation} (x + a)^n = \sum_{k=0}^{n}\binom{n}{k} x^k a^{n-k} \end{equation} 
+# >
+# > -----------------------------
+# 
